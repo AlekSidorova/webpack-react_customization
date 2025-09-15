2667fcf14a10c88c3eb2cb574c8258c2b719b08e
SAWYER SAWYER <mehaleka240399@yandex.ru>
2025-09-16 00:31:05 +0300

    feat(webpack): закончена сборка webpack react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import style from './index.module.scss';
-import './index.scss';
-import './assets/images/dog.jpg';
-import { ReactComponent as ReactLogo } from './assets/images/react.svg'; 
+import style from './index.module.scss'
+import './index.scss'
+import './assets/images/dog.jpg'
+import { ReactComponent as ReactLogo } from './assets/images/react.svg'
 
-console.log(style); //использование объекта style важно для попадания стилей в css-бандл 
+console.log(style) //использование объекта style важно для попадания стилей в css-бандл
